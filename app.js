219b1fa68daaf1b887f5add919b3928adcbfe178
AyalaJDann951 <96157722+AyalaJDann951@users.noreply.github.com>
2022-06-04 16:40:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,20 @@ document.addEventListener('DOMContentLoaded', (){
   const scoreDisplay = document.querySelector('.countScore')
   const startBtn = document.querySelector('.start')
   const livesDisplay = document.querySelector('.countLives')
+  
+  const  WIDTH = 30
+  let currentIndex
+  let appleIndex
+  let currentSnake
+  let direction
+  let score, lives, speed, intervalTime, interval, stopGame
+  
+  lives = 0
+  score = 0
+
+  function control(e){
+    alert(e.keyCode)
+  }
+
+  document.keyEventListener('keydown', control)
 })
