3314bb15b0768d6084f820315fecb083b1ea978f
AyalaJDann951 <96157722+AyalaJDann951@users.noreply.github.com>
2022-06-04 16:35:12 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (){
+  const squares = document.querySelectorAll('.grid div')
+  const grid = document.querySelector('.grid')
+  const scoreDisplay = document.querySelector('.countScore')
+  const startBtn = document.querySelector('.start')
+  const livesDisplay = document.querySelector('.countLives')
+})
